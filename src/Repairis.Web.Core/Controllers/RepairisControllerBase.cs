e89d08f5f34eb8cac20a46b4f892bac2c84fd697
Dmytro Kuzmin <374645@mail.muni.cz>
2017-05-15 05:30:21 +0200

    Disable exception throwing when ModelState is not valid

--- src/Repairis.Web.Core/Controllers/RepairisControllerBase.cs
+++ src/Repairis.Web.Core/Controllers/RepairisControllerBase.cs
@@ -1,9 +1,11 @@
 using Abp.AspNetCore.Mvc.Controllers;
 using Abp.IdentityFramework;
+using Abp.Runtime.Validation;
 using Microsoft.AspNetCore.Identity;
 
 namespace Repairis.Controllers
 {
+    [DisableValidation]
     public abstract class RepairisControllerBase: AbpController
     {
         protected RepairisControllerBase()
