8ed228fd110823753b28d1f010cc5544c3b20dfd
Dmytro Kuzmin <374645@mail.muni.cz>
2017-06-23 18:09:45 +0200

    Add WarrantyExpirationChecker, which automatically updates the order statuses

---
+++ src/Repairis.Core/Orders/WarrantyExpirationChecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Threading.BackgroundWorkers;
+using Abp.Threading.Timers;
+
+namespace Repairis.Orders
+{
+    public class WarrantyExpirationChecker : PeriodicBackgroundWorkerBase, ISingletonDependency
+    {
+        private readonly IRepository<Order, long> _orderRepository;
+
+        public WarrantyExpirationChecker(AbpTimer timer, IRepository<Order, long> orderRepository)
+            : base(timer)
+        {
+            _orderRepository = orderRepository;
+            Timer.Period = 60 * 60 * 1000; //1 hour
+        }
+
+        [UnitOfWork]
+        protected override void DoWork()
+        {
+            using (CurrentUnitOfWork.DisableFilter(AbpDataFilters.MayHaveTenant))
+            {
+                var now = DateTime.Now;
+
+                var ordersWithExpiredWarranty = 
+                    _orderRepository.GetAllList(x => x.OrderStatus == OrderStatusEnum.OnWarranty &&
+                                                          x.WarrantyExpirationDate <= now);
+
+                foreach (var order in ordersWithExpiredWarranty)
+                {
+                    order.OrderStatus = OrderStatusEnum.Closed;
+                }
+
+                CurrentUnitOfWork.SaveChanges();
+            }
+        }
+    }
+}
